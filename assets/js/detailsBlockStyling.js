b566405a1e8ec0309ddc17c6af8a40774fc46fd0
Volodymyr Kravchuk <vkravch@softserveinc.com>
2017-01-27 18:40:50 +0200

    mobile 3 pages done

---
+++ assets/js/detailsBlockStyling.js
@@ -0,0 +1,3 @@
+$('.info-block a.action').addClass('btn');
+$('.info-block tr td:first-child').addClass('col-md-4');
+$('.info-block tr td:last-child').addClass('col-md-8');
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  var slideout = new Slideout({
-    'panel': document.getElementById('wrap'),
-    'menu': document.getElementById('nav'),
-    'side': 'right',
-  });
+    var slideout = new Slideout({
+        'panel': document.getElementById('wrap'),
+        'menu': document.getElementById('nav'),
+        'side': 'right',
+    });
 
-	$(".navbar-toggle").click(function(event) {
-		slideout.toggle();
-	});
+    $(".navbar-toggle").click(function(event) {
+    	slideout.toggle();
+    });
 });
